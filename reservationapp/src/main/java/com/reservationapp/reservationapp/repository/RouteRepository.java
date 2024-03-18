9b934b9191a5d9477b7c278618c8e0f10b36131f
Manish21-coder <raghavmanishprakash@gmail.com>
2024-03-18 17:54:39 +0530

    Added bus details,routes,subroutes

---
+++ reservationapp/src/main/java/com/reservationapp/reservationapp/repository/RouteRepository.java
@@ -0,0 +1,15 @@
+package com.reservationapp.reservationapp.repository;
+
+import com.reservationapp.reservationapp.entity.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+  Route findByBusId(long BusId);
+
+  List<Route> findByFromLocationAndToLocationAndFromDate(String fromLocation,String toLocation,String fromDate);
+
+}
+
